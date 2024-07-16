e74bb7baaf20b6afb4ee249a6b7bacf8ac6d2d42
shifxshf <shifanasshafeek01@gmail.com>
2024-07-16 15:26:19 +0530

    click cover

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ let songindex = 0;
 let audioelement = new Audio('songs/Jai Ho.mp3');
 let masterplay = document.getElementById('masterplay');
 let myprogressbar = document.getElementById('myprogressbar');
-let gif = document.getElementById('gif');
+// let gif = document.getElementById('gif');
 let songitems = Array.from(document.getElementsByClassName('songitem'));
 
 let songs = [
     {songname: "Puthu Vellai Mazhai", filepath: "songs/Puthu Vellay.mp3", coverpath: "covers/roja.jpg"},
-    {songname: "Jiya Jale", filepath: "songs/Jiya Jale.mp3.mp3", coverpath: "covers/dilse.jpg"},
+    {songname: "Jiya Jale", filepath: "songs/Jiya Jale.mp3", coverpath: "covers/dilse.jpg"},
     {songname: "Jai Ho", filepath: "songs/Jai Ho.mp3", coverpath: "covers/slumdogmillionare.jpg"},
     // {songname: "puthu vellai mazhai", filepath: "4.mp3", coverpath: "roja.jpg"},
     // {songname: "puthu vellai mazhai", filepath: "5.mp3", coverpath: "roja.jpg"},
 ];
 
 songitems.forEach((element, i)=>{
-    console.log(element, i);
+    // console.log(element, i);
     element.getElementsByTagName("img")[0].src = songs[i].coverpath;
     element.getElementsByClassName("songname")[0].innerText = songs[i].songname;
 
@@ -29,11 +29,12 @@ masterplay.addEventListener('click', ()=>{
     if(audioelement.paused || audioelement.currentTime<=0){
         audioelement.play();
         masterplay.textContent = 'pause';
-        gif.style.opacity = 1;
+        currentsong.innerText = songs[songindex].songname;
+        // gif.style.opacity = 1;
     }else{
         audioelement.pause();
         masterplay.textContent = 'play_arrow';
-        gif.style.opacity = 0;
+        // gif.style.opacity = 0;
     }
 });
 
@@ -49,3 +50,16 @@ audioelement.addEventListener('timeupdate', ()=>{
 myprogressbar.addEventListener('change', ()=>{
     audioelement.currentTime = myprogressbar.value * audioelement.duration/100;
 });
+
+Array.from(document.getElementsByClassName('cover')).forEach((element, index)=>{
+    element.addEventListener('click', () => {
+        audioelement.src = songs[index].filepath;
+        audioelement.load();
+        audioelement.play().then(() => {
+            masterplay.textContent = 'pause';
+            songindex = index; // Update the song index
+            currentsong.innerText = songs[songindex].songname; // Update current song name
+        });
+    });
+});
+
